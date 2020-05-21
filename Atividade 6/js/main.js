c54308ddc7716d39a3a205391b008f90416aafe8
gabrielpaulino <gabriel.paulino104@gmail.com>
2020-05-20 21:11:37 -0300

    Atividade 6

---
+++ Atividade 6/js/main.js
@@ -0,0 +1,24 @@
+//Modo 1 para criação de objetos:
+
+var Aluno1 = new Object();
+Aluno1.RA = "0030481913014";
+Aluno1.Nome = "Gabriel";
+
+//alert("Nome: " + Aluno1.Nome + "\nRA: " + Aluno1.RA);
+
+//Modo 2 para criação de objetos:
+
+var Aluno2 = {};
+Aluno2.RA = "0030481913015";
+Aluno2.Nome = "Leonardo";
+
+//alert("Nome: " + Aluno2.Nome + "\nRA: " + Aluno2.RA);
+
+//Modo 3 para criação de objetos:
+
+var Aluno3 = {
+    RA: "0030481913016",
+    Nome: "Alexandre"
+};
+
+//alert("Nome: " + Aluno3.Nome + "\nRA: " + Aluno3.RA);
