48dab66f942bbdfd019e59d2bcd9c572dcb3dda1
gabrielpaulino <gabriel.paulino104@gmail.com>
2020-05-27 23:24:02 -0300

    Atividade 8

---
+++ Atividade 8/js/main.js
@@ -0,0 +1,11 @@
+function mOver(obj) {
+    obj.src = "imgs/janelaaberta.jpg";
+}
+
+function mOut(obj) {
+    obj.src = "imgs/janelafechada.jpg";
+}
+
+function mClick(obj) {
+    obj.src = "imgs/janelaquebrada.jpg";
+}
